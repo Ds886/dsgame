3b1062aa5a5fae7e464de39f925dd3e67662e453
Bar Angel <bar.ang16@gmail.com>
2025-05-02 17:23:04 +0300

    bug fix - augmented vector should have Z coord 1, not 0.

--- source/polygon.c
+++ source/polygon.c
@@ -4,15 +4,15 @@ Triangle isoscelesTriangle(float base, float height) {
   Triangle res;
   res.a.x = -base / 2;
   res.a.y = -height / 3;
-  res.a.z = 0;
+  res.a.z = 1;
 
   res.b.x = base / 2;
   res.b.y = -height / 3;
-  res.b.z = 0;
+  res.b.z = 1;
 
   res.c.x = 0;
   res.c.y = 2 * height / 3;
-  res.c.z = 0;
+  res.c.z = 1;
 
   return res;
 }
