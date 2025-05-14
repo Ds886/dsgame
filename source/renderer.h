e8a1b0abaf9f8ff81d1d63ec57e36eaff30c9a5b
Bar Angel <bar.ang16@gmail.com>
2025-05-14 13:31:39 +0300

    useful color macros

--- source/renderer.h
+++ source/renderer.h
@@ -9,6 +9,16 @@
 #define FLOAT_TO_5BITS(n) ((uint16_t)(((1 << MAX_COLOR_BITS)-1)*n))
 #define COLOR_TO_15BIT(col) (RGB15(FLOAT_TO_5BITS(VGET(col, 0)), FLOAT_TO_5BITS(VGET(col, 1)), FLOAT_TO_5BITS(VGET(col, 2))))
 
+#define MAKE_COLOR(x, y, z) make_vec(x, y, z)
+#define RED      MAKE_COLOR(1, 0, 0)
+#define GREEN    MAKE_COLOR(0, 1, 0)
+#define BLUE     MAKE_COLOR(0, 0, 1)
+#define YELLOW   MAKE_COLOR(1, 1, 0)
+#define MAGENTA  MAKE_COLOR(0, 1, 1)
+#define WHITE    MAKE_COLOR(1, 1, 1)
+#define GREY     MAKE_COLOR(0.2, 0.2, 0.2)
+#define BROWN    MAKE_COLOR(0.5, 0.1, 0)
+
 typedef vector Color;
 
 void renderPolygonTransformed(Polygon *poly, vector pos,  matrix m, Color color);
