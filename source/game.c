58c4c3f2fddfee8a1dd226c95b6174563a1cf776
Bar Angel <bar.ang16@gmail.com>
2025-05-17 18:23:25 +0300

    remove rendering of bounding box and collision box

--- source/game.c
+++ source/game.c
@@ -287,10 +287,6 @@ Game *gameLogic(Game *game, uint16_t keys) {
 }
 
 void renderGameObj(GameObj *obj) {
-  Color rect_color = make_vec(0.2, 0.2, 0.2);
-  Polygon rect = boundingBox(&obj->polygon);
-  
-  renderPolygon(&rect, obj->position, rect_color);
   renderPolygon(&obj->polygon, obj->position, obj->color);
 }
 
@@ -306,19 +302,6 @@ Game *gameRender(Game *game) {
     GameObj *astro = &game->astroids[i].obj;
     if (astro->alive)
       renderGameObj(astro);
-
-    Polygon poly;
-    if (checkObjCollision(&game->astroids[i].obj, &game->ship->obj, &poly)) {
-      renderPolygon(&poly, ZERO_VEC, make_vec(0, 1, 1));
-      printf("Collision with astroid %d!\n", i);
-    }
-
-    for (int j=0 ;j < game->ship->num_shoots; j++){
-      if (checkObjCollision(&game->astroids[i].obj, &game->ship->shoots[j].obj, &poly)) {
-        renderPolygon(&poly, ZERO_VEC, make_vec(1, 1, 0));
-        printf("hit astroid %d if shoot %d!\n", i, j);
-      }
-    }
   }
 
   return game;  
